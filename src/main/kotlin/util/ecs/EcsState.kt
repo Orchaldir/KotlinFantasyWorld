1e71fd4295bb222faf892f059f2b57ecce9c0f79
Orchaldir <orchaldir@gmail.com>
2020-04-09 12:14:22 +0200

    [#12] EcsState.get() only requires generic type

--- src/main/kotlin/util/ecs/EcsState.kt
+++ src/main/kotlin/util/ecs/EcsState.kt
@@ -1,12 +1,19 @@
 package util.ecs
 
 import util.ecs.storage.ComponentStorage
+import kotlin.reflect.KClass
 
-class EcsState(private val componentMap: Map<Int, ComponentStorage<*>>) {
-    fun <T> get(type: Int): ComponentStorage<T>? {
+class EcsState(private val componentMap: Map<KClass<*>, ComponentStorage<*>>) {
+
+    fun <T> get(type: KClass<*>): ComponentStorage<T>? {
         val storage = componentMap[type]
 
         @Suppress("UNCHECKED_CAST")
-        return storage as ComponentStorage<T>
+        return storage as ComponentStorage<T>?
     }
+
+    inline fun <reified T : Any> get(): ComponentStorage<T>? {
+        return get(T::class)
+    }
+
 }
--- src/test/kotlin/util/ecs/EcsStateTest.kt
+++ src/test/kotlin/util/ecs/EcsStateTest.kt
@@ -5,14 +5,25 @@ import assertk.assertions.isSameAs
 import io.mockk.mockk
 import org.junit.Test
 import util.ecs.storage.ComponentStorage
+import kotlin.test.assertNull
 
 class ComponentMapTest {
 
     @Test
-    fun `Test get() with known id`() {
+    fun `Test get() with existing type`() {
         val c0 = mockk<ComponentStorage<Int>>()
-        val state = EcsState(mapOf(0 to c0))
+        val c1 = mockk<ComponentStorage<String>>()
+        val state = EcsState(mapOf(Int::class to c0, String::class to c1))
 
-        assertThat(state.get<Int>(0)).isSameAs(c0)
+        assertThat(state.get<Int>()).isSameAs(c0)
+        assertThat(state.get<String>()).isSameAs(c1)
+    }
+
+    @Test
+    fun `Test get() with non-existing type`() {
+        val c0 = mockk<ComponentStorage<Int>>()
+        val state = EcsState(mapOf(Int::class to c0))
+
+        assertNull(state.get<String>())
     }
 }
